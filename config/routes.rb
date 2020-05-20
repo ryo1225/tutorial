a71a3a9f0b34b83bbf6e00d29f594f579a1b30e5
ryo <xxxyyz3332211@gmail.com>
2020-05-20 14:41:04 +0000

    [Add]7.3.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
 
   resources :microposts
+  resources :users
   #resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
